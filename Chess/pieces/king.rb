814f9feb9a9c51c8b605de3c9dee05f31d4da6e5
micah-jaffe <micah.jaffe95@gmail.com>
2018-10-30 10:29:14 -0700

    Write #symbol and #move_diffs for King and Knight classes

---
+++ Chess/pieces/king.rb
@@ -0,0 +1,11 @@
+class King < Piece 
+  
+  def symbol
+    @color == :W ? "\u2654" : "\u265a"
+  end
+  
+  def move_diffs
+    [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]]
+  end
+  
+end
--- Chess/pieces/knight.rb
+++ Chess/pieces/knight.rb
@@ -1,5 +1,11 @@
 class Knight < Piece
   
+  def symbol
+    @color == :W ? "\u2658" : "\u265e"
+  end
   
+  def move_diffs
+    [[-2, -1], [-2, 1], [2, -1], [2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2]]
+  end
   
 end
